2bdc07f9f2c96a7fd24150653966f85fc8af61d4
pa2tie <myadsagain@gmail.com>
2018-10-23 08:44:15 +0400

    working algorithm

--- lib/src/wallet/wallet_component.dart
+++ lib/src/wallet/wallet_component.dart
@@ -20,12 +20,12 @@ class WalletComponent implements OnInit {
   WalletComponent(this._billService);
 
   var bills = [
-    {'n': 1, 'c': 1},
     {'n': 5, 'c': 1},
-    {'n': 10, 'c': 1}
+    {'n': 10, 'c': 1},
+    {'n': 15, 'c': 1}
   ];
 
-  var capacity = 10;
+  var capacity = 45;
 
   dynamic cashout(bills, capacity) {
 
@@ -33,11 +33,10 @@ class WalletComponent implements OnInit {
 
     getLast() {
       var lastRow = memo[memo.length - 1];
-      return lastRow[lastRow - 1];
+      return lastRow[lastRow.length - 1];
     }
 
     getSolution(row, cap) {
-      print('getSol: ');
       const NO_SOLUTION = {
         'amount': 0,
         'count': 0, 
@@ -59,23 +58,27 @@ class WalletComponent implements OnInit {
 
       var lastSolutionCurrentRow = new Map.from(row >= 0 ? ((remaining - 1) >= 0 && (remaining - 1) < cap ? memo[row][remaining - 1] : NO_SOLUTION) : NO_SOLUTION);
 
-
-
-      var lastCount = lastSolution['count'];
-      var lastSubCount = lastSubSolution['count'];
-      var lastCurrentRowCount = lastSolutionCurrentRow['count'];
-
-      var lastAmount = lastSolution['amount'];
-      var lastSubAmount = lastSubSolution['amount'];
-      var lastCurrentRowAmount = lastSolutionCurrentRow['amount'];
-      
-      var newCount = lastSubCount + lastItem['c'];
-      var newCurrentRowCount = lastCurrentRowCount + lastItem['c'];
-      
-      var newAmount = lastSubAmount + lastItem['n'];
-      var newCurrentRowAmount = lastCurrentRowAmount + lastItem['n'];
-
       List<dynamic> lastSolutions = [lastSubSolution, lastSolutionCurrentRow];
+      lastSolutions = lastSolutions.where((a) => (a['amount'] + lastItem['n']) == cap).toList();
+
+      List<dynamic> newLastSolutions = [];
+
+      lastSolutions = lastSolutions.map((a) {
+          var _lastSubSet = List.of(a['subset'].map((el) => new Map.from(el)));
+          var subset = _lastSubSet.length > 0 ? _lastSubSet.firstWhere((el) => el['n'] == lastItem['n'], orElse: () => []) : _lastSubSet;
+
+          var newAmount = a['amount'] + lastItem['n'];
+          var newCount = a['count'] + lastItem['c'];
+
+          if (subset.length > 0) {
+            subset['c']++;
+            return { 'amount': newAmount, 'count': newCount, 'subset': _lastSubSet};
+          } else {
+            _lastSubSet.add(new Map.from(lastItem));
+            return { 'amount': newAmount, 'count': newCount, 'subset': _lastSubSet};
+          }
+      }).toList();
+
       lastSolutions.sort((a, b) {
         if (a['subset'].length == b['subset'].length) {
           return (a['count'] < b['count']) ? -1 : 1;
@@ -84,36 +87,7 @@ class WalletComponent implements OnInit {
         }
       });
 
-      lastSolutions = lastSolutions.where((a) => a['n'] == cap).toList();
-
-      
-      if (newAmount == cap) {
-        var _lastSubSet = List.of(lastSubSolution['subset'].map((el) => new Map.from(el)));
-        var subset = _lastSubSet.length > 0 ? _lastSubSet.firstWhere((el) => el['n'] == lastItem['n'], orElse: () => []) : _lastSubSet;
-
-        if (subset.length > 0) {
-          subset['c']++;
-          return { 'amount': newAmount, 'count': newCount, 'subset': _lastSubSet};
-        } else {
-          _lastSubSet.add(new Map.from(lastItem));
-          return { 'amount': newAmount, 'count': newCount, 'subset': _lastSubSet};
-        }
-
-      } else if (newCurrentRowAmount == cap) {
-        var _lastCurRowSet = List.of(lastSolutionCurrentRow['subset'].map((el) => new Map.from(el)));
-        var subset = _lastCurRowSet.length > 0 ? _lastCurRowSet.firstWhere((el) => el['n'] == lastItem['n'], orElse: () => {}) : _lastCurRowSet;
-
-        if (subset.length > 0) {
-          subset['c']++;
-          return { 'amount': newCurrentRowAmount, 'count': newCurrentRowCount, 'subset': _lastCurRowSet};
-        } else {
-          _lastCurRowSet.add(new Map.from(lastItem));
-          return { 'amount': newCurrentRowAmount, 'count': newCurrentRowCount, 'subset': _lastCurRowSet};
-        }
-
-      } else {
-        return lastSolution;
-      }
+      return (lastSolutions.length > 0 ? lastSolutions[0] : lastSolution);
 
     }
 
@@ -122,12 +96,12 @@ class WalletComponent implements OnInit {
       memo.add(row);
       for(var cap = 1; cap <= capacity; cap++) {
         memo[i].add(getSolution(i, cap));
-        print(memo[i][cap - 1]);
+        // print(memo[i][cap - 1]);
       }
     }
 
-    print(memo[0]);
-    return memo;
+    print(getLast());
+    return getLast();
     
   }
 
